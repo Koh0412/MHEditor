79de44d8c49d3f77bc1ed496d36633534cd12062
kohei.hujita <point.out92@gmail.com>
2020-11-16 18:13:24 +0900

    add editor comp to app and monaco lib install

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Editor from './Editor';
 import TestComponent from './TestComponent';
 
 function App(): JSX.Element {
   return (
     <div>
       <TestComponent name="test" />
+      <Editor />
     </div>
   );
 }
--- src/renderer/components/Editor.tsx
+++ src/renderer/components/Editor.tsx
@@ -1,3 +1,14 @@
 import React from 'react';
 
-class Editor extends React.Component {}
+class Editor extends React.Component {
+  render(): JSX.Element {
+    return (
+      <>
+        <div>editor zone</div>
+        <div id="editable"></div>
+      </>
+    );
+  }
+}
+
+export default Editor;
